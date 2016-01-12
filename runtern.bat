6dd49cf30554b92b0deda79202daf3d56350491e
Jozef Sovcik <jozef@sovcik.net>
2016-01-12 18:42:59 +0100

    Cmdline supports classpath wildcard expansion
    So no need to explicitly list all libraries

--- runtern.bat
+++ runtern.bat
@@ -3,9 +3,7 @@ set curDir=%cd%
 set scriptDir="%~dp0"
 pushd %scriptDir%
 
-set cpth=Tern.jar
-set cpth=%cpth%;bluecove-2.1.1-SNAPSHOT-63.jar
-set cpth=%cpth%;ev3j-0.1.1.jar
+set cpth=Tern.jar;lib\*
 
 echo Starting Tern
 echo Folder %scriptDir%
